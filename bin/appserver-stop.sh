c97473d73693902e85359c821c9512effc756bbe
Marjohann <marjundendi115@gmail.com>
2023-08-10 15:06:16 +0800

    updates

--- bin/appserver-stop.sh
+++ bin/appserver-stop.sh
@@ -4,7 +4,6 @@ cd ..
 BASE_DIR=`pwd`
 
 
-cd $BASE_DIR/appserver/waterworks && docker-compose down
 
 cd $BASE_DIR/appserver/epayment && docker-compose down
 
--- bin/start.sh
+++ bin/start.sh
@@ -22,7 +22,6 @@ cd $BASE_DIR/appserver/etracs && docker-compose -f docker-compose.yml -f docker-
 
 cd $BASE_DIR/appserver/epayment && docker-compose -f docker-compose.yml -f docker-compose-ext.yml up -d
 
-cd $BASE_DIR/appserver/waterworks && docker-compose up -d
 
 
 cd $RUN_DIR
